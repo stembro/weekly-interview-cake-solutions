71c7edbf07ae0b786fd20e6c16b025741a4cdd87
Stephen Brown <ontheheap@gmail.com>
2016-12-20 12:47:05 -0700

    Update solution.js

--- Bracket Validator/solution.js
+++ Bracket Validator/solution.js
@@ -9,8 +9,7 @@ let result = (function(input) {
     let stack = [];
     let openers = ['{', '[', '('];
     let closers = ['}', ']', ')'];
-    let pairCounts = [0]
-    
+
     for (let c of input) {
         if (openers.indexOf(c) >= 0) {
             stack.push(c);
@@ -28,9 +27,7 @@ let result = (function(input) {
         }
     }
     
-    return stack.length === 0 && pairCounts.reduce(function(a,b) {
-        return a && b % 2 === 0;
-    }, true);
+    return stack.length === 0;
     
 })("{[()]}");
 
