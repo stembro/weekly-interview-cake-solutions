a84fae34e0df3d1cf190d1632183eb8bd9bff341
Stephen Brown <ontheheap@gmail.com>
2017-03-29 01:44:09 +0000

    inflight entertainment

---
+++ Inflight Entertainment/solution.js
@@ -0,0 +1,24 @@
+// Stephen Brown
+// ontheheap@gmail.com
+// Interview Cake - Reverse Words
+// https://www.interviewcake.com/question/javascript/inflight-entertainment
+
+'use strict';
+
+let result = (function(flightLength, movieDurations) {
+    
+    let movieTimesNeededToMatchFlightLength = Object.create(null);
+    
+    for (let movieDuration of movieDurations) {
+        if (movieDuration in movieTimesNeededToMatchFlightLength) {
+            return true;
+        }
+        
+        Object.defineProperty(movieTimesNeededToMatchFlightLength, flightLength - movieDuration, {});
+    }
+    
+    return false;
+
+})(120, [110, 45, 11, 85, 10]);
+
+console.log(result);
