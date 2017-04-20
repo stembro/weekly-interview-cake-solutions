703767bb92221a014eaf8a6bac84db402a1e999e
Stephen Brown <ontheheap@gmail.com>
2017-04-20 03:29:57 +0000

    reverse a linked list (in place)

---
+++ Reverse A Linked List/solution.js
@@ -0,0 +1,81 @@
+// Stephen Brown
+// ontheheap@gmail.com
+// Interview Cake - Reverse A Linked List
+// https://www.interviewcake.com/question/javascript/reverse-linked-list
+
+'use strict';
+
+let result = (function(testInput) {
+    
+    class LinkedListNode {
+        constructor(value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    
+    function stringToList(str) {
+        let head = new LinkedListNode(str[0]);
+        str.split('').slice(1).reduce((lastListItem, char) => {
+            let node = new LinkedListNode(char);
+            lastListItem.next = node;
+            return node;
+        }, head);
+        
+        return head;
+    }
+    
+    function listToString(head) {
+        let curr = head;
+        let str = '';
+        while (curr !== null) {
+            str += curr.value;
+            curr = curr.next;
+        }
+        
+        return str;
+    }
+    
+    function printList(head) {
+        let curr = head;
+        let arr = [];
+        while (curr !== null) {
+            arr.push(curr.value);
+            curr = curr.next;
+        }
+        console.log(arr.join(''));
+    }
+    
+    function reverseListInPlace(head) {
+
+        // set prevNode to null
+        // set currentNode to head
+        
+        // start loop while current.next is not null
+        //  -- set nextNode to current.next
+        //  -- set currentNode.next to prevNode
+        //  -- prevNode to currentNode
+        //  -- currentNode = nextNode
+        
+        let currentNode = head;
+        let prevNode = null;
+        
+        while (currentNode !== null) {
+            let nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+
+            if (nextNode === null) {
+                break;
+            }
+            currentNode = nextNode;
+        }
+        
+        return currentNode;
+    }
+    
+    return listToString(reverseListInPlace(stringToList(testInput)));
+    
+})("hello world!");
+
+console.log(result);
